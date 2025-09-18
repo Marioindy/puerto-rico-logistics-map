40882d2ef22eb1d4fd83903c8d206045ed4abd1a
Jesiel Carro <jesielcarro@gmail.com>
2025-09-18 15:52:29 -0400

    Fixed Query error in Convex part

--- convex/functions/logistics/tracking.ts
+++ convex/functions/logistics/tracking.ts
@@ -1,9 +1,9 @@
-import { query } from "convex/server";
+import { query } from "../../_generated/server";
 
 // Placeholder query until Convex schema + ingestion are finalized.
 export const listActiveShipments = query({
   args: {},
-  handler: async (ctx) => {
+  handler: async (ctx: any) => {
     void ctx;
     return [];
   }
